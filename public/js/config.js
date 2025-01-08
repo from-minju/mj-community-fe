1ca7dd1b2d721f7864da05a444edac4a98592fe3
jenny.kang <deco1202@naver.com>
2025-01-08 17:15:43 +0900

    refactor: 서버요청경로 /api/추가

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@ export const HOST = "localhost";
 
 export const PORT = 8000;
 
-export const API_BASE_URL = `http://${HOST}:3000`;
+export const API_BASE_URL = `http://${HOST}:3000/api`;
 
 export const API_IMAGE_URL = `${API_BASE_URL}/uploads`
 
